f0fbdffd7f6227716e1ad8575dc355afa2a34b88
angelinalubnina <angelinalubnina@mail.ru>
2023-08-23 19:56:59 +0400

    changed the create function in ColorController

--- controllers/colorController.js
+++ controllers/colorController.js
@@ -1,18 +1,38 @@
 const ColorModel = require('../models/color')
 
 class ColorController {
+    // async create(req, res) {
+    //     const { name, code } = req.body
+    //     const existColor = await ColorModel.findOne({
+    //         name, code
+    //     })
+    //     if(existColor) {
+    //         return res.json('Цвет уже существует')
+    //     }
+    //     const color = await ColorModel.create({
+    //         name, code
+    //     })
+    //     return res.json(color)
+    // }
+
     async create(req, res) {
-        const { name, code } = req.body
-        const existColor = await ColorModel.findOne({
-            name, code
-        })
-        if(existColor) {
-            return res.json('Цвет уже существует')
+        const { colors } = req.body
+        // [{name:'', code:''},{name:'', code:''}]
+        console.log(colors)
+        for (const color of colors) {
+            const { name, code } = color;
+            const existColor = await ColorModel.findOne({
+                name, code
+            })
+            if(existColor) {
+                return res.json('Цвет уже существует')
+            } else {
+                const color = await ColorModel.create({
+                    name, code
+                })
+            }
         }
-        const color = await ColorModel.create({
-            name, code
-        })
-        return res.json(color)
+        // return res.json(color)
     }
 
     async getAll(req, res) {
--- models/color.js
+++ models/color.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const ColorSchema = new Schema({
     name: { type: String, required: true },
-    code: { type: String, unique: true, require: true },
+    code: { type: String, required: true },
 });
 
 module.exports = model('Color', ColorSchema)
